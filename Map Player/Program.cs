94b6e021bc924d55f149e05a5e680ad217b04e90
jayden <jayden@cumallover.me>
2020-08-29 22:52:33 +0100

    start on map player

---
+++ Map Player/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map_Player
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                return 0;
+            }
+            try
+            {
+
+            }
+            catch
+            {
+                return 0;
+            }
+            return 0;
+        }
+    }
+}
