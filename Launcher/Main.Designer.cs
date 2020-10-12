fa2bd43a03ce734556992c8c5d72127d8107e3e9
jayden <jayden@cumallover.me>
2020-10-12 23:16:43 +0100

    Launcher WoKRiNG

--- Launcher/Main.Designer.cs
+++ Launcher/Main.Designer.cs
@@ -29,12 +29,14 @@
         private void InitializeComponent()
         {
             this.MainContainer = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.Changelog = new System.Windows.Forms.WebBrowser();
-            this.DownloadProgress = new System.Windows.Forms.ProgressBar();
-            this.LaunchButton = new System.Windows.Forms.Button();
-            this.DownloadInfo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.NoTasks = new System.Windows.Forms.Label();
             this.VersionSelect = new System.Windows.Forms.ComboBox();
+            this.DownloadInfo = new System.Windows.Forms.Label();
+            this.LaunchButton = new System.Windows.Forms.Button();
+            this.DownloadProgress = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
             this.MainContainer.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -49,8 +51,18 @@
             this.MainContainer.Size = new System.Drawing.Size(560, 300);
             this.MainContainer.TabIndex = 0;
             // 
+            // Changelog
+            // 
+            this.Changelog.Location = new System.Drawing.Point(0, 0);
+            this.Changelog.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Changelog.Name = "Changelog";
+            this.Changelog.Size = new System.Drawing.Size(560, 230);
+            this.Changelog.TabIndex = 2;
+            // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.NoTasks);
             this.panel1.Controls.Add(this.VersionSelect);
             this.panel1.Controls.Add(this.DownloadInfo);
             this.panel1.Controls.Add(this.LaunchButton);
@@ -61,44 +73,16 @@
             this.panel1.Size = new System.Drawing.Size(560, 64);
             this.panel1.TabIndex = 1;
             // 
-            // Changelog
+            // NoTasks
             // 
-            this.Changelog.Location = new System.Drawing.Point(0, 0);
-            this.Changelog.MinimumSize = new System.Drawing.Size(20, 20);
-            this.Changelog.Name = "Changelog";
-            this.Changelog.Size = new System.Drawing.Size(560, 230);
-            this.Changelog.TabIndex = 2;
-            // 
-            // DownloadProgress
-            // 
-            this.DownloadProgress.Location = new System.Drawing.Point(0, 42);
-            this.DownloadProgress.Name = "DownloadProgress";
-            this.DownloadProgress.Size = new System.Drawing.Size(560, 18);
-            this.DownloadProgress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
-            this.DownloadProgress.TabIndex = 0;
-            this.DownloadProgress.Value = 69;
-            this.DownloadProgress.Visible = false;
-            // 
-            // LaunchButton
-            // 
-            this.LaunchButton.Location = new System.Drawing.Point(460, 8);
-            this.LaunchButton.Name = "LaunchButton";
-            this.LaunchButton.Size = new System.Drawing.Size(100, 32);
-            this.LaunchButton.TabIndex = 1;
-            this.LaunchButton.Text = "Launch";
-            this.LaunchButton.UseVisualStyleBackColor = true;
-            // 
-            // DownloadInfo
-            // 
-            this.DownloadInfo.AutoSize = true;
-            this.DownloadInfo.BackColor = System.Drawing.Color.Transparent;
-            this.DownloadInfo.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.DownloadInfo.Location = new System.Drawing.Point(1, 29);
-            this.DownloadInfo.Name = "DownloadInfo";
-            this.DownloadInfo.Size = new System.Drawing.Size(154, 13);
-            this.DownloadInfo.TabIndex = 2;
-            this.DownloadInfo.Text = "Downloading release zip..(69%)";
-            this.DownloadInfo.Visible = false;
+            this.NoTasks.BackColor = System.Drawing.Color.Transparent;
+            this.NoTasks.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.NoTasks.Location = new System.Drawing.Point(240, 43);
+            this.NoTasks.Name = "NoTasks";
+            this.NoTasks.Size = new System.Drawing.Size(60, 16);
+            this.NoTasks.TabIndex = 4;
+            this.NoTasks.Text = "No Tasks";
+            this.NoTasks.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // VersionSelect
             // 
@@ -114,11 +98,55 @@
             "1.5",
             "1.4.2f",
             "1.4.2"});
-            this.VersionSelect.Location = new System.Drawing.Point(330, 14);
+            this.VersionSelect.Location = new System.Drawing.Point(300, 14);
             this.VersionSelect.Name = "VersionSelect";
-            this.VersionSelect.Size = new System.Drawing.Size(125, 21);
+            this.VersionSelect.Size = new System.Drawing.Size(150, 21);
             this.VersionSelect.TabIndex = 3;
             this.VersionSelect.Text = "Latest (1.6)";
+            // 
+            // DownloadInfo
+            // 
+            this.DownloadInfo.AutoSize = true;
+            this.DownloadInfo.BackColor = System.Drawing.Color.Transparent;
+            this.DownloadInfo.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.DownloadInfo.Location = new System.Drawing.Point(1, 29);
+            this.DownloadInfo.Name = "DownloadInfo";
+            this.DownloadInfo.Size = new System.Drawing.Size(154, 13);
+            this.DownloadInfo.TabIndex = 2;
+            this.DownloadInfo.Text = "Downloading release zip..(69%)";
+            this.DownloadInfo.Visible = false;
+            // 
+            // LaunchButton
+            // 
+            this.LaunchButton.Location = new System.Drawing.Point(460, 8);
+            this.LaunchButton.Name = "LaunchButton";
+            this.LaunchButton.Size = new System.Drawing.Size(100, 32);
+            this.LaunchButton.TabIndex = 1;
+            this.LaunchButton.Text = "Launch";
+            this.LaunchButton.UseVisualStyleBackColor = true;
+            this.LaunchButton.Click += new System.EventHandler(this.LaunchButton_Click);
+            // 
+            // DownloadProgress
+            // 
+            this.DownloadProgress.Location = new System.Drawing.Point(0, 42);
+            this.DownloadProgress.Name = "DownloadProgress";
+            this.DownloadProgress.Size = new System.Drawing.Size(560, 18);
+            this.DownloadProgress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.DownloadProgress.TabIndex = 0;
+            this.DownloadProgress.Value = 69;
+            this.DownloadProgress.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(255, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Version:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Main
             // 
@@ -130,6 +158,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Main";
             this.Text = "SS Quantum Editor";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.MainContainer.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -146,5 +175,7 @@
         private System.Windows.Forms.Label DownloadInfo;
         private System.Windows.Forms.Button LaunchButton;
         private System.Windows.Forms.ProgressBar DownloadProgress;
+        private System.Windows.Forms.Label NoTasks;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Launcher/Main.cs
+++ Launcher/Main.cs
@@ -3,18 +3,144 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using Ionic.Zip;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Launcher
 {
     public partial class Main : Form
     {
+        private WebClient wc = new WebClient();
         public Main()
         {
             InitializeComponent();
+        }
+
+        public List<Release> releaseList;
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            wc.Headers.Add("User-Agent: krmeet");
+            var releases = wc.DownloadString("https://api.github.com/repos/David20122/Sound-Space-Quantum-Editor/releases");
+            releaseList = JsonSerializer.Deserialize<List<Release>>(releases);
+            VersionSelect.Items.Clear();
+            VersionSelect.Items.Add("Latest");
+            VersionSelect.Text = "Latest";
+            foreach (var release in releaseList)
+            {
+                VersionSelect.Items.Add(release.name);
+            }
+        }
+
+        private async void DownloadRelease(Release release)
+        {
+            DownloadInfo.Text = "";
+            DownloadProgress.Value = 0;
+            NoTasks.Visible = false;
+            DownloadInfo.Visible = true;
+            DownloadProgress.Visible = true;
+            LaunchButton.Enabled = false;
+            VersionSelect.Enabled = false;
+            try
+            {
+                DownloadInfo.Text = "Getting SSQE directory";
+                var appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                var ssqeDir = Path.Combine(appdata, "SSQE");
+                var verDir = Path.Combine(ssqeDir, "Versions/" + release.name);
+                if (!Directory.Exists(ssqeDir))
+                {
+                    DownloadInfo.Text = "Creating new SSQE directory";
+                    Directory.CreateDirectory(ssqeDir);
+                    Directory.CreateDirectory(Path.Combine(ssqeDir, "Versions"));
+                }
+                else
+                {
+                    if (!Directory.Exists(Path.Combine(ssqeDir, "Versions")))
+                    {
+                        DownloadInfo.Text = "Creating new versions directory";
+                        Directory.CreateDirectory(Path.Combine(ssqeDir, "Versions"));
+                    }
+                }
+                DownloadInfo.Text = "Checking if version exists";
+                if (Directory.Exists(verDir) && File.Exists(Path.Combine(verDir,"Sound Space Quantum Editor.exe")))
+                {
+                    DownloadInfo.Text = "Version exists, running";
+                    var startInfo = new ProcessStartInfo();
+                    startInfo.FileName = Path.Combine(verDir, "Sound Space Quantum Editor.exe");
+                    startInfo.WorkingDirectory = verDir;
+                    Process.Start(startInfo);
+                    Close();
+                } else
+                {
+                    WebClient dl = new WebClient();
+                    dl.Headers.Add("User-Agent: krmeet");
+                    DownloadInfo.Text = "Downloading version "+release.name;
+                    dl.DownloadProgressChanged += Dl_DownloadProgressChanged;
+                    var tempFile = Path.Combine(ssqeDir, "dltemp_" + release.name + "._temp");
+                    await dl.DownloadFileTaskAsync(new Uri(release.assets.First().browser_download_url), tempFile);
+                    DownloadProgress.Value = 0;
+                    DownloadInfo.Text = "Unzipping";
+                    var zip = ZipFile.Read(tempFile);
+                    zip.ExtractProgress += Zip_ExtractProgress;
+                    zip.ExtractAll(Path.Combine(ssqeDir, "Versions"));
+                    if (Directory.Exists(verDir) && File.Exists(Path.Combine(verDir, "Sound Space Quantum Editor.exe")))
+                    {
+                        DownloadInfo.Text = "Version exists, running";
+                        var startInfo = new ProcessStartInfo();
+                        startInfo.FileName = Path.Combine(verDir, "Sound Space Quantum Editor.exe");
+                        startInfo.WorkingDirectory = verDir;
+                        Process.Start(startInfo);
+                        Close();
+                    }
+                }
+            } catch (Exception e) {
+                throw e;
+            }
+            NoTasks.Visible = true;
+            DownloadInfo.Visible = false;
+            DownloadProgress.Visible = false;
+            LaunchButton.Enabled = true;
+            VersionSelect.Enabled = true;
+        }
+
+        private void Zip_ExtractProgress(object sender, ExtractProgressEventArgs e)
+        {
+            if (e.TotalBytesToTransfer > 0)
+            {
+                DownloadProgress.Value = (int)e.BytesTransferred / (int)e.TotalBytesToTransfer;
+            }
+            DownloadInfo.Text = "Unzipping " + DownloadProgress.Value.ToString() + "%";
+        }
+
+        private void Dl_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            DownloadProgress.Value = e.ProgressPercentage;
+            DownloadInfo.Text = "Downloading release from GitHub " + e.ProgressPercentage.ToString() + "% (" + e.BytesReceived.ToString() + "B/" + e.TotalBytesToReceive.ToString() + "B)";
+        }
+
+        private void LaunchButton_Click(object sender, EventArgs e)
+        {
+            if(VersionSelect.Text == "Latest")
+            {
+                DownloadRelease(releaseList.First());
+            } else
+            {
+                foreach (Release release in releaseList)
+                {
+                    if (release.name == VersionSelect.Text)
+                    {
+                        DownloadRelease(release);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
