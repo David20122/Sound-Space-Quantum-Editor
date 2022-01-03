79bfa3fd9a23790c75b950db7b0ef3ffedbb8741
Avibah <58798677+Avibah@users.noreply.github.com>
2022-01-03 17:36:23 -0600

    ver change

--- Blox Saber Editor/Properties/AssemblyInfo.cs
+++ Blox Saber Editor/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("9a405608-105f-473a-8f17-9d8c05f82b19")]
-[assembly: AssemblyVersion("1.7.2.1")]
-[assembly: AssemblyFileVersion("1.7.2.1")]
+[assembly: AssemblyVersion("1.7.2.2")]
+[assembly: AssemblyFileVersion("1.7.2.2")]
