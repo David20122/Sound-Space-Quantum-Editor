25761c37b7551b50c5d1514a763c9a7c4a11223a
Avibah <58798677+Avibah@users.noreply.github.com>
2022-02-27 21:16:25 -0600

    repaired indenting

--- Blox Saber Editor/BookmarkSetup.cs
+++ Blox Saber Editor/BookmarkSetup.cs
@@ -11,43 +11,43 @@ using Sound_Space_Editor.Gui;
 
 namespace Sound_Space_Editor
 {
-	public partial class BookmarkSetup : Form
-	{
-    public static BookmarkSetup inst;
-    public BookmarkSetup()
+    public partial class BookmarkSetup : Form
     {
-      inst = this;
-      InitializeComponent();
-      ResetList();
-    }
-    public void ResetList()
-    {
-      BookmarkList.Rows.Clear();
-      foreach (var point in GuiSliderTimeline.Bookmarks)
-      {
-        BookmarkList.Rows.Add(point.Name, point.MS.ToString());
-      }
-    }
-		private void UpdateList(object sender, DataGridViewCellEventArgs e)
-		{
-      GuiSliderTimeline.Bookmarks.Clear();
-      for (int i = 0; i < BookmarkList.Rows.Count; i++)
-			{
-        var bookmark = BookmarkList.Rows[i];
-        if (bookmark.Cells[0].Value != null && bookmark.Cells[1].Value != null)
+        public static BookmarkSetup inst;
+        public BookmarkSetup()
         {
-          var bmrk = new Bookmark((string)bookmark.Cells[0].Value, int.Parse((string)bookmark.Cells[1].Value));
-          GuiSliderTimeline.Bookmarks.Add(bmrk);
+            inst = this;
+            InitializeComponent();
+            ResetList();
         }
-			}
-		}
+        public void ResetList()
+        {
+            BookmarkList.Rows.Clear();
+            foreach (var point in GuiSliderTimeline.Bookmarks)
+            {
+                BookmarkList.Rows.Add(point.Name, point.MS.ToString());
+            }
+        }
+        private void UpdateList(object sender, DataGridViewCellEventArgs e)
+        {
+            GuiSliderTimeline.Bookmarks.Clear();
+            for (int i = 0; i < BookmarkList.Rows.Count; i++)
+            {
+                var bookmark = BookmarkList.Rows[i];
+                if (bookmark.Cells[0].Value != null && bookmark.Cells[1].Value != null)
+                {
+                    var bmrk = new Bookmark((string)bookmark.Cells[0].Value, int.Parse((string)bookmark.Cells[1].Value));
+                    GuiSliderTimeline.Bookmarks.Add(bmrk);
+                }
+            }
+        }
 
-		private void CurrentButton_Click(object sender, EventArgs e)
-		{
-      if (BookmarkList.SelectedCells.Count > 0)
-			{
-        BookmarkList.SelectedCells[0].OwningRow.Cells[1].Value = EditorWindow.Instance.currentTime.TotalMilliseconds.ToString();
-			}
-		}
-	}
+        private void CurrentButton_Click(object sender, EventArgs e)
+        {
+            if (BookmarkList.SelectedCells.Count > 0)
+            {
+                BookmarkList.SelectedCells[0].OwningRow.Cells[1].Value = EditorWindow.Instance.currentTime.TotalMilliseconds.ToString();
+            }
+        }
+    }
 }
