8491f404a4020b2bdee3a41dcf478c952a7185b2
Avibah <58798677+Avibah@users.noreply.github.com>
2022-02-28 22:13:28 -0600

    fixed base approach rate

--- Blox Saber Editor/Gui/GuiGrid.cs
+++ Blox Saber Editor/Gui/GuiGrid.cs
@@ -143,7 +143,7 @@ namespace Sound_Space_Editor.Gui
 				var x = rect.X + note.X * cellSize + gap / 2;
 				var y = rect.Y + note.Y * cellSize + gap / 2;
 
-				var progress = (float)Math.Pow(1 - Math.Min(1, (note.Ms - audioTime) * ApproachRate / 10.0 / 1000.0 / (0.2f + (editor.Tempo.Value > 15 ? (editor.Tempo.Value - 16) * 2 + 16 : editor.Tempo.Value) * 0.05f)), 2);
+				var progress = (float)Math.Pow(1 - Math.Min(1, (note.Ms - audioTime) * ApproachRate / 10.0 / 750.0 / (0.2f + (editor.Tempo.Value > 15 ? (editor.Tempo.Value - 16) * 2 + 16 : editor.Tempo.Value) * 0.05f)), 2);
 				var noteRect = new RectangleF(x, y, noteSize, noteSize);
 				OpenTK.Graphics.Color4 colora = new OpenTK.Graphics.Color4(note.Color.R, note.Color.G, note.Color.B, progress * 0.15f);
 				GL.Color4(colora);
