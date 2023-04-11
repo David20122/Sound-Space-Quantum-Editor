63fe4da57c7ef4f4a77b0821d97594024d1b0114
Avibah <redstoneminecraft45@gmail.com>
2023-04-11 08:57:05 -0500

    upload updater source

---
+++ SSQE Updater/SSQE Updater/Program.cs
@@ -0,0 +1,127 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+
+namespace SSQE_Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentPath = Directory.GetCurrentDirectory();
+            var currentVersion = "";
+            if (File.Exists("Sound Space Quantum Editor.exe"))
+                currentVersion = FileVersionInfo.GetVersionInfo("Sound Space Quantum Editor.exe").FileVersion;
+
+            var newVersion = CheckVersion();
+            var file = $"SSQE{newVersion}.zip";
+
+            if (newVersion != "")
+            {
+                Console.WriteLine("Starting download...");
+                
+                try
+                {
+                    WebClient.DownloadFile($"https://github.com/David20122/Sound-Space-Quantum-Editor/releases/download/{newVersion}/SSQE{newVersion}.zip", file);
+                    ExtractFile();
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to download new editor version");
+                    Quit();
+                }
+            }
+
+            string[] GetOverwrites()
+            {
+                try
+                {
+                    var overwriteList = WebClient.DownloadString("https://raw.githubusercontent.com/David20122/Sound-Space-Quantum-Editor/1.9%2B_rewrite/updater_overwrite");
+
+                    return overwriteList.Split('\n');
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to fetch overrides");
+                    Quit();
+                }
+
+                return Array.Empty<string>();
+            }
+
+            string CheckVersion()
+            {
+                try
+                {
+                    var redirect = WebClient.GetRedirect("https://github.com/David20122/Sound-Space-Quantum-Editor/releases/latest");
+
+                    if (!string.IsNullOrWhiteSpace(redirect))
+                    {
+                        var version = redirect[(redirect.LastIndexOf("/") + 1)..];
+
+                        if (version != currentVersion)
+                            return version;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to check version");
+                    Quit();
+                }
+
+                return "";
+            }
+
+            void KillProcess()
+            {
+                foreach (var process in Process.GetProcessesByName("Sound Space Quantum Editor"))
+                    process.Kill();
+
+                Thread.Sleep(500);
+            }
+
+            bool IsInOverwrites(string[] list, string fileName)
+            {
+                foreach (var line in list)
+                    if (fileName == line)
+                        return true;
+
+                return false;
+            }
+
+            void ExtractFile()
+            {
+                var overwriteList = GetOverwrites();
+
+                Console.WriteLine("Completed, extracting...");
+
+                KillProcess();
+
+                using (ZipArchive archive = ZipFile.OpenRead(file))
+                {
+                    foreach (var entry in archive.Entries)
+                    {
+                        try
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(Path.Combine(currentPath, entry.FullName)) ?? "");
+                            entry.ExtractToFile(Path.Combine(currentPath, entry.FullName), IsInOverwrites(overwriteList, entry.FullName));
+                        }
+                        catch { }
+                    }
+                }
+
+                Console.WriteLine("Completed, launching...");
+
+                File.Delete(file);
+                Process.Start("Sound Space Quantum Editor");
+
+                Quit();
+            }
+
+            void Quit()
+            {
+                Thread.Sleep(1500);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ SSQE Updater/SSQE Updater/WebClient.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+
+namespace SSQE_Updater
+{
+    internal class WebClient
+    {
+        private static readonly HttpClient client = new();
+        private static readonly HttpClient redirectClient = new(new HttpClientHandler()
+        {
+            AllowAutoRedirect = false
+        });
+
+        public static string GetRedirect(string url)
+        {
+            var final = "";
+
+            var result = Task.Run(async () =>
+            {
+                using HttpResponseMessage response = await redirectClient.GetAsync(url);
+
+                if (response.StatusCode == HttpStatusCode.Redirect)
+                    final = (response.Headers.Location ?? new Uri("")).ToString();
+            }).ContinueWith(t =>
+            {
+                return final;
+            });
+
+            return result.Result;
+        }
+
+        public static string DownloadString(string url)
+        {
+            var result = Task.Run(async () =>
+            {
+                using HttpResponseMessage response = await client.GetAsync(url);
+                using HttpContent content = response.Content;
+
+                return content.ReadAsStringAsync().Result;
+            });
+
+            if (result.Result != null)
+                return result.Result;
+            else
+                throw new WebException();
+        }
+
+        public static void DownloadFile(string url, string location)
+        {
+            var result = Task.Run(async () =>
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+                using HttpResponseMessage response = await client.SendAsync(request);
+                using HttpContent content = response.Content;
+
+                var stream = content.ReadAsStreamAsync().Result;
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    using FileStream fs = new(location, FileMode.Create);
+                    await stream.CopyToAsync(fs);
+                }
+
+                return response.StatusCode;
+            });
+
+            if (result.Result != HttpStatusCode.OK)
+                throw new WebException($"({(int)result.Result}) {result.Result}");
+        }
+    }
+}
