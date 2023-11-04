cff50bd5467c3fad40de7c8e4c1cec5c80cab9a1
Avibah <redstoneminecraft45@gmail.com>
2023-11-03 21:03:02 -0500

    add indicator when placing notes with no bpm

--- Editor/New SSQE/Misc/UndoRedoManager.cs
+++ Editor/New SSQE/Misc/UndoRedoManager.cs
@@ -11,6 +11,8 @@ namespace New_SSQE
 
         public int _index = -1;
 
+        private DateTime? prevTime;
+
         public void Add(string label, Action undo, Action redo, bool runRedo = true, bool reload = false)
         {
             try
@@ -25,6 +27,19 @@ namespace New_SSQE
 
                 if (runRedo && _index < actions.Count && _index >= 0)
                     actions[_index].Redo?.Invoke();
+
+
+                // cause people hate bpm now?
+                if (label == "ADD NOTE" && runRedo && MainWindow.Instance.TimingPoints.Count == 0 && MainWindow.Instance.CurrentWindow is GuiWindowEditor editor)
+                {
+                    DateTime curTime = DateTime.Now;
+
+                    if (prevTime == null || (curTime - prevTime)?.TotalMilliseconds >= 5000)
+                    {
+                        editor.ShowToast("Please use BPM!", Settings.settings["color2"]);
+                        prevTime = curTime;
+                    }
+                }
             }
             catch (Exception ex)
             {
